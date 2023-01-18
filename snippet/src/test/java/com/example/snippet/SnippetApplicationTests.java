27c2a6ac4c07e0500dcd157408ae562dfe24a3ef
Sathish V <sathishvp2495@gmail.com>
2023-01-18 10:13:39 +0530

    Created Project using start.spring.io

---
+++ snippet/src/test/java/com/example/snippet/SnippetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snippet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
